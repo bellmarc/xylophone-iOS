ffb7f974f8dc44cc04494d24cd3b28dbd2e3c355
Marcielo Bell <marcielobell@gmail.com>
2020-08-31 14:25:17 -0700

    Modified xylophone Swift project. In progress for sound file functionality.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -3,19 +3,47 @@
 //  Xylophone
 //
 //  Created by Angela Yu on 28/06/2019.
+//  Edited by Marc
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
+import AVFoundation //added for wav
+
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+//Pre-built Apple APIs manual
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        print("Red got pressed")
+    }
     
-
+    @IBAction func keyPressedOrange(_ sender: UIButton) {
+        print("Orange pressed")
+    }
+    
+    //adding Sound functionality
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else {
+            return
+        }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: <#T##URL#>, fileTypeHint: AVFileType.wav.rawValue)
+            guard let player = player else {return}
+            player.play()
+        }
+        catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
